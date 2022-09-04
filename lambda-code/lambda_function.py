5280abfd582faa129ec3f0f327738c9b7771a294
Raj Kamal <rakamal@BLR.ALLEGISINDIA.COM>
2022-09-05 03:30:55 +0530

    payload

--- lambda-code/lambda_function.py
+++ lambda-code/lambda_function.py
@@ -1,13 +1,15 @@
 import logging
 import psycopg2
 import base64
+import json
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 def lambda_handler(event, context):
     for record in event['Records']:
        #Kinesis data is base64 encoded so decode here
        payload=base64.b64decode(record["kinesis"]["data"])
-       print("Decoded payload: " + str(payload))
+       new_payload = json.loads(payload)
+       print(new_payload)
     # conn = psycopg2.connect(
     # host="34.202.163.219",
     # database="airflow",
