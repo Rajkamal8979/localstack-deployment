d35a13eafd5b527bc7d0bee46201d3cb7237b788
Raj Kamal <rakamal@BLR.ALLEGISINDIA.COM>
2022-09-05 00:42:03 +0530

    lambda

--- lambda-code/lambda_function.py
+++ lambda-code/lambda_function.py
@@ -2,13 +2,12 @@ import logging
 import psycopg2
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
-
-conn = psycopg2.connect(
+def lambda_handler(event, context):
+    conn = psycopg2.connect(
     host="34.202.163.219",
     database="airflow",
     user="airflow",
     password="airflow")
-def lambda_handler(event, context):
     logging.info('Lambda in LocalStack')
     cur = conn.cursor()
     cur.execute('SELECT version()')
