94621288dbdce42eff91a1bc922508e10cd3804f
Raj Kamal <rakamal@BLR.ALLEGISINDIA.COM>
2022-09-04 22:42:07 +0530

    psycopg2 deped

--- lambda-code/lambda_function.py
+++ lambda-code/lambda_function.py
@@ -1,11 +1,19 @@
 import logging
-
+import psycopg2
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
-
+conn = psycopg2.connect(
+    host="34.202.163.219",
+    database="airflow",
+    user="airflow",
+    password="airflow")
 def lambda_handler(event, context):
     logging.info('Lambda in LocalStack')
+    cur = conn.cursor()
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    logging.info(db_version)
     return {
-        "message": "Hello User!"
+        "message": db_version
     }
