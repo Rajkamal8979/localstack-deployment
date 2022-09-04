85e7d3385b4eed8eb476d98260aa94b9970c622c
Raj Kamal <rakamal@BLR.ALLEGISINDIA.COM>
2022-09-05 02:49:26 +0530

    kinesis code

--- lambda-code/lambda_function.py
+++ lambda-code/lambda_function.py
@@ -1,18 +1,23 @@
 import logging
 import psycopg2
+import base64
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 def lambda_handler(event, context):
-    conn = psycopg2.connect(
-    host="34.202.163.219",
-    database="airflow",
-    user="airflow",
-    password="airflow")
-    logging.info('Lambda in LocalStack')
-    cur = conn.cursor()
-    cur.execute('SELECT version()')
-    db_version = cur.fetchone()
-    logging.info(db_version)
-    return {
-        "message": db_version
-    }
+    for record in event['Records']:
+       #Kinesis data is base64 encoded so decode here
+       payload=base64.b64decode(record["kinesis"]["data"])
+       print("Decoded payload: " + str(payload))
+    # conn = psycopg2.connect(
+    # host="34.202.163.219",
+    # database="airflow",
+    # user="airflow",
+    # password="airflow")
+    # logging.info('Lambda in LocalStack')
+    # cur = conn.cursor()
+    # cur.execute('SELECT version()')
+    # db_version = cur.fetchone()
+    # logging.info(db_version)
+    # return {
+    #     "message": db_version
+    # }
